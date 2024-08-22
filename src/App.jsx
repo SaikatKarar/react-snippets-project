5189d3acb9a6738c43fc896c4e489a1f8f4b2195
SaikatKarar <kararsaikat@gmail.com>
2024-08-22 11:49:51 +0530

    add JSON data

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,9 @@ import Header from './component/Header'
 function App() {
   return (
     <div>
-      <Header />
+      <div className="fixed top-0 w-full z-50 bg-white inline-block">
+        <Header />
+      </div>
       <div className='min-h-[calc(100vh-136px)]'>
         <Outlet />
       </div>
